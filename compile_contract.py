48fecd734a24e8eaefd6f51cd46445a7636097d0
jefftrojan <j.dauda@alustudent.com>
2023-12-28 03:05:00 +0200

    compile comntract

---
+++ compile_contract.py
@@ -0,0 +1,20 @@
+from pyteal import *
+
+from marketplace_contract import Product
+
+if __name__ == "__main__":
+    approval_program = Product.approval_program()
+    clear_program = Product.clear_program()
+
+    # Mode.Application specifies that this is a smart contract
+    compiled_approval = compileTeal(approval_program, Mode.Application, version=6)
+    print(compiled_approval)
+    with open('marketplace_approval.teal', 'w') as teal:
+        teal.write(compiled_approval)
+        teal.close()
+
+    compiled_clear = compileTeal(approval_program, Mode.Application, version=6)
+    print(compiled_clear)
+    with open('marketplace_clear.teal', 'w') as teal:
+        teal.write(compiled_clear)
+        teal.close()
